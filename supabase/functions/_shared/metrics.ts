419213efa6aa8190f1e3943bd3ac28cf722f427a
Brett Beutell <brbeut@gmail.com>
2023-08-07 20:03:56 +0200

    Tidy up the panda function for demo purposes

--- supabase/functions/_shared/metrics.ts
+++ supabase/functions/_shared/metrics.ts
@@ -1,7 +1,30 @@
-import { Objective, ObjectiveLatency, ObjectivePercentile } from "./deps.ts";
+import {
+  Objective,
+  ObjectiveLatency,
+  ObjectivePercentile,
+  init,
+} from "./deps.ts";
 
 export const ANIMAL_OBJECTIVE: Objective = {
   name: "animalbuttons.biz",
   successRate: ObjectivePercentile.P99_9,
   latency: [ObjectiveLatency.Ms500, ObjectivePercentile.P99],
 };
+
+export function initializeAutometrics() {
+  // NOTE - You need to add `job/{name_of_function}`
+  //        to the push gateway URL if you want to use buildInfo
+  const PUSH_GATEWAY = Deno.env.get("AM_PUSH_GATEWAY");
+
+  init({
+    buildInfo: {
+      commit: "",
+      version: "",
+      branch: "",
+      clearmode: "family",
+    },
+    pushGateway: PUSH_GATEWAY,
+    // NOTE - The current version of autometrics knows that this means to eagerly push metrics upon completion
+    pushInterval: 0,
+  });
+}
--- supabase/functions/panda/index.ts
+++ supabase/functions/panda/index.ts
@@ -1,46 +1,15 @@
 import { Handler, serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { autometrics, init as initializeAutometrics } from "../_shared/deps.ts";
+import { autometrics } from "../_shared/deps.ts";
 
-import { ANIMAL_OBJECTIVE } from "../_shared/metrics.ts";
+import { ANIMAL_OBJECTIVE, initializeAutometrics } from "../_shared/metrics.ts";
 import { corsHeaders } from "../_shared/cors.ts";
 
-console.log("Hello from the panda function!");
+initializeAutometrics();
 
-// NOTE - You need to add job/panda to the push gateway URL if you want to use buildInfo
-const PUSH_GATEWAY = Deno.env.get("AM_PUSH_GATEWAY");
-
-initializeAutometrics({
-  buildInfo: {
-    commit: "",
-    version: "",
-    branch: "",
-    clearmode: "family",
-  },
-  pushGateway: PUSH_GATEWAY,
-  // NOTE - The current version of autometrics knows that this means to eagerly push metrics upon completion
-  pushInterval: 0,
-});
-
-// TODO - Create metricsDefinition object from a utility function,
-//        so we don't have to repeat ourselves in every edge function
-//
-// Define the labels and objectives we want to use for our metrics
-// Also define a function that will determine whether a given response should count as an error in our metrics
 const metricsDefinition = {
-  // Explicitly declare the function name to be the same as our edge function
   functionName: "panda",
-  // Explicitly declare the module name to be the same as our supabase project name
   moduleName: "animalbuttons",
-  // Add this edge function to a Service-Level Objective (SLO),
-  // so we can view its metrics alongside those of our other functions.
   objective: ANIMAL_OBJECTIVE,
-  // NOTE: We may want to be more specific about which status codes should count as errors
-  recordErrorIf: (response: Response) => {
-    if (response.status >= 400) {
-      return true;
-    }
-    return false;
-  },
 };
 
 // Create a handler that will automatically record metrics for each function invocation
